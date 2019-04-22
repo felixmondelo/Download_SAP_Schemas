309f9bf93a689b60b749064491f3c06fd35522e7
felixmondelo <FMondelo@kabel.es>
2019-04-22 11:25:03 +0000

    Initial version

---
+++ Download_SAP_Schemas/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Download_SAP_Schemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "YOUR URL";
+            string xsdPath = Path.GetTempPath();
+
+            MainAsync(url, xsdPath).Wait();
+
+            Console.WriteLine("Press RETURN to finish...");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string url, string destinationPath)
+        {            
+            var httpClient = new HttpClient();
+            
+            var content = new StringContent("", Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(url, content);
+
+            response.EnsureSuccessStatusCode();
+
+            string responseText = await response.Content.ReadAsStringAsync();
+
+            var convertedValue = JsonConvert.DeserializeObject<ListOfSchemas>(responseText);
+
+            
+            foreach (Schemas s in convertedValue.Schemas)
+            {
+                string name = s.Name;
+                XmlDocument xmlSchema = new XmlDocument();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] clearString = Convert.FromBase64String(s.Content);
+                    ms.Write(clearString, 0, clearString.Length);
+                    ms.Position = 0;
+                    xmlSchema.Load(ms);
+
+                    string destinationFilename = string.Format("{0}{1}.xsd", destinationPath, name);                    
+                    xmlSchema.Save(destinationFilename);
+                }                
+            }                        
+        }
+    }
+}
---
+++ Download_SAP_Schemas/Schemas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Download_SAP_Schemas
+{    
+    class ListOfSchemas
+    {
+        public List<Schemas> Schemas { get; set; }
+    }
+
+    class Schemas
+    {
+        public Schemas()
+        {
+
+        }
+
+        public Schemas (string name, string content)
+        {
+            Name = name;
+            Content = content;
+        }
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+
+}
